db33c752c321ffe6c3228ed726173c7d87790c97
ericak11 <kantor.erica@gmail.com>
2014-08-14 14:06:45 -0400

    finished problem 3

--- number_3_prime_factor.rb
+++ number_3_prime_factor.rb
@@ -1,18 +1,17 @@
-########Largest prime factor - #3 ################################
-# require 'Prime'
+# ########Largest prime factor - #3 ################################
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600,851,475,143 ?
+require 'Prime'
+num_array = (50000000..500000000).to_a
+prime_array = []
+
+num_array.each do |num|
+test_num = (600851475143.0/num).to_f
+  if 600851475143/num == test_num && Prime.prime?(test_num) #checks if number is prime and evenly divides into 6....
+    prime_array.push(test_num)
+  end
+end
 
 
-# prime_array = []
-
-# sum = 0
-
-# Prime.each(300425737571) do |prime|
-#   prime_array.push(prime)
-# end
-
-# prime_array.each do |num|
-#   if (600851475143/num).is_a? Fixnum
-
-#   end
-# end
-# binding.pry
+# Answer - 6857
